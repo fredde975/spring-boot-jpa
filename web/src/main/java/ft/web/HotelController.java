a6829fe6ca1a07ba338e90050472de95fbdc8f16
Fredrik Thernelius <fredrik.thernelius@gmail.com>
2018-04-19 14:41:12 +0200

    Added endpoint to add hotels

--- web/src/main/java/ft/web/HotelController.java
+++ web/src/main/java/ft/web/HotelController.java
@@ -3,10 +3,12 @@ package ft.web;
 import ft.domain.Hotel;
 import ft.persistance.HotelRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class HotelController {
@@ -18,12 +20,18 @@ public class HotelController {
     }
 
     @GetMapping("/hotels")
-    public List<Hotel> getHotels(){
+    public List<Hotel> getHotels() {
         try {
             return hotelRepository.findAll();
         } catch (Exception e) {
             return new ArrayList<>();
         }
     }
+
+    @GetMapping("/addHotel")
+    public void addHotel(@RequestParam(value="name", required=true) String name) {
+        hotelRepository.save(new Hotel(name, (new Random().nextInt(5)), true));
+    }
+
 
 }
